ae93dd0f027f13ec986d04b0eb0f01d42ede7edf
Fabio Salvatori <eng.fabio.salvatori@gmail.com>
2025-09-19 18:17:37 +0200

    get tcp sockets info from kernel

--- main.c
+++ main.c
@@ -12,7 +12,68 @@
 #include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
+
+// for socket comunication
+#include <asm/types.h>
+#include <linux/netlink.h>
+#include <sys/socket.h>
+
+// for sock_diag netlink subsystem
+#include <linux/sock_diag.h>
+/*#include <linux/unix_diag.h> [> for UNIX domain sockets <]*/
+#include <linux/inet_diag.h> /* for IPv4 and IPv6 sockets */
+#include <linux/tcp.h>
+
+// in order to manipulate rtnetlink messages
+// see https://man7.org/linux/man-pages/man3/rtnetlink.3.html
+#include <asm/types.h>
+#include <linux/rtnetlink.h>
+#include <sys/socket.h>
+
+#ifndef TCPF_ALL
+
+/*
+ * Can't find header file that contains the following definitions of tcp states.
+ * Should ne in <netinet/tcp_states.h> is not part of glibc’s “standard”
+ * includes like <netinet/tcp.h>. It’s provided by the linux-headers package
+ * (the UAPI export from your kernel). If your system doesn’t have
+ * <netinet/tcp_states.h> try to update linux headers
+ */
+typedef enum {
+  TCP_ESTABLISHED = 1,
+  TCP_SYN_SENT,
+  TCP_SYN_RECV,
+  TCP_FIN_WAIT1,
+  TCP_FIN_WAIT2,
+  TCP_TIME_WAIT,
+  TCP_CLOSE,
+  TCP_CLOSE_WAIT,
+  TCP_LAST_ACK,
+  TCP_LISTEN,
+  TCP_CLOSING, /* Now a valid state */
+  TCP_NEW_SYN_RECV,
+  TCP_MAX_STATES /* Leave at the end! */
+} tcp_status;
+
+#define TCPF_ESTABLISHED (1 << TCP_ESTABLISHED)
+#define TCPF_SYN_SENT (1 << TCP_SYN_SENT)
+#define TCPF_SYN_RECV (1 << TCP_SYN_RECV)
+#define TCPF_FIN_WAIT1 (1 << TCP_FIN_WAIT1)
+#define TCPF_FIN_WAIT2 (1 << TCP_FIN_WAIT2)
+#define TCPF_TIME_WAIT (1 << TCP_TIME_WAIT)
+#define TCPF_CLOSE (1 << TCP_CLOSE)
+#define TCPF_CLOSE_WAIT (1 << TCP_CLOSE_WAIT)
+#define TCPF_LAST_ACK (1 << TCP_LAST_ACK)
+#define TCPF_LISTEN (1 << TCP_LISTEN)
+#define TCPF_CLOSING (1 << TCP_CLOSING)
+
+#define TCPF_ALL                                                               \
+  (TCPF_ESTABLISHED | TCPF_SYN_SENT | TCPF_SYN_RECV | TCPF_FIN_WAIT1 |         \
+   TCPF_FIN_WAIT2 | TCPF_TIME_WAIT | TCPF_CLOSE | TCPF_CLOSE_WAIT |            \
+   TCPF_LAST_ACK | TCPF_LISTEN | TCPF_CLOSING)
+#endif
 
 /*
  * The following function convert ip address:
@@ -60,6 +121,7 @@ int main(int argc, char *argv[]) {
     enum connection_type type;
     address local;
     address remote;
+    tcp_status status;
     process *proc;
   } connection;
 
@@ -98,6 +160,8 @@ int main(int argc, char *argv[]) {
         address_converter(token, a->remote.ip);
       } else if (j == 4) {
         a->remote.port = (int)strtol(token, NULL, 16);
+      } else if (j == 5) {
+        a->status = (int)strtol(token, NULL, 16);
       } else if (j == 13) {
         a->inode = (char *)malloc(strlen(token) + 1);
         strcpy(a->inode, token);
@@ -200,7 +264,7 @@ int main(int argc, char *argv[]) {
               fclose(cfd);
 
               /*printf("PROCESS: %s \tINODE: %s \tCOMMAND: %s \n", d->d_name,*/
-                     /*connections[x]->inode, connections[x]->proc->comm);*/
+              /*connections[x]->inode, connections[x]->proc->comm);*/
               free(result);
               break;
             }
@@ -215,17 +279,153 @@ int main(int argc, char *argv[]) {
   }
   closedir(dir_main);
 
-  printf("n\tlocal\tport\tremote\tport\tinode\tpid\tcommand\n");
+  printf("n\tlocal\tport\tremote\tport\ts_type\tinode\tstatus\tpid\tcommand\n");
   int z;
   for (z = 0; z < number_of_connections; z++) {
-    printf("%d\t%s\t%d\t%s\t%d\t%u\t%s", z, connections[z]->local.ip,
+    printf("%d\t%s\t%d\t%s\t%d\t%u\t%s\t%d", z, connections[z]->local.ip,
            connections[z]->local.port, connections[z]->remote.ip,
-           connections[z]->remote.port,connections[z]->type, connections[z]->inode);
+           connections[z]->remote.port, connections[z]->type,
+           connections[z]->inode, connections[z]->status);
     if (connections[z]->proc != NULL) {
       printf("\t%s\t%s", connections[z]->proc->pid, connections[z]->proc->comm);
     }
     printf("\n");
   }
+
+  // retrieveing information about specific sockets from the kernel
+  /*for (z = 0; z < number_of_connections; z++) {*/
+    /*if (connections[z]->proc != NULL) {*/
+      /*if (strcmp(connections[z]->proc->comm, "spotify") == 0) {*/
+        /*printf("%d\n", z);*/
+        /*break;*/
+      /*}*/
+    /*}*/
+  /*}*/
+  /*if (z == number_of_connections) {*/
+    /*puts("process not exists");*/
+    /*exit(0);*/
+  /*}*/
+  /*z = 31;*/
+
+  int sfd;
+  if ((sfd = socket(AF_NETLINK, SOCK_RAW, NETLINK_SOCK_DIAG)) == -1) {
+    perror("during socket creation: ");
+    return EXIT_FAILURE;
+  }
+
+  while (1) {
+    struct sockaddr_nl nladdr = {.nl_family = AF_NETLINK};
+
+    // Build netlink header + request
+    // see https://man7.org/linux/man-pages/man7/netlink.7.html
+    char buf[8192];
+    struct nlmsghdr *nlh = (struct nlmsghdr *)buf;
+    nlh->nlmsg_len = NLMSG_LENGTH(sizeof(struct inet_diag_req_v2));
+    nlh->nlmsg_type = SOCK_DIAG_BY_FAMILY;
+    nlh->nlmsg_flags = NLM_F_REQUEST | NLM_F_DUMP;
+    nlh->nlmsg_seq = 12345;
+    nlh->nlmsg_pid = 0;
+
+    // see https://man7.org/linux/man-pages/man7/sock_diag.7.html
+    /*
+    struct inet_diag_sockid id;
+    id.idiag_sport = connections[z]->local.port;
+    inet_pton(AF_INET, connections[z]->local.ip, id.idiag_src);
+    id.idiag_dport = connections[z]->remote.port;
+    inet_pton(AF_INET, connections[z]->local.ip, id.idiag_dst);
+    id.idiag_if = 0;
+    */
+
+    /*struct inet_diag_req_v2 req;*/
+    struct inet_diag_req_v2 *req = (struct inet_diag_req_v2 *)NLMSG_DATA(nlh);
+    memset(req, 0, sizeof(*req));
+    req->sdiag_family = AF_INET;
+    req->sdiag_protocol = IPPROTO_TCP;
+    req->idiag_ext = (1 << (INET_DIAG_INFO - 1));
+    req->pad = 0;
+    req->idiag_states = TCPF_ALL; // filter sockets by status
+    /*req->id = id;*/
+
+    struct iovec iov = {nlh, nlh->nlmsg_len};
+    struct msghdr msg = {&nladdr, sizeof(nladdr), &iov, 1, NULL, 0, 0};
+
+    if (sendmsg(sfd, &msg, 0) < 0) {
+      perror("sendmsg");
+      return 1;
+    }
+
+    // Read responses
+    int goon = 1;
+    while (goon) {
+      int len = recv(sfd, buf, sizeof(buf), 0);
+      if (len <= 0)
+        break;
+
+      for (nlh = (struct nlmsghdr *)buf; NLMSG_OK(nlh, len);
+           nlh = NLMSG_NEXT(nlh, len)) {
+        if (nlh->nlmsg_type == NLMSG_DONE) {
+          goon = 0;
+          break;
+        }
+        if (nlh->nlmsg_type == NLMSG_ERROR) {
+          fprintf(stderr, "netlink error\n");
+          goon = 0;
+          break;
+        }
+        struct inet_diag_msg *diag = (struct inet_diag_msg *)NLMSG_DATA(nlh);
+        printf("Socket: src port %u, dst port %u, state %u, inode: %d\n",
+               ntohs(diag->id.idiag_sport), ntohs(diag->id.idiag_dport),
+               diag->idiag_state, diag->idiag_inode);
+        /*printf("%d\n", diag->id.idiag_if);*/
+        /*printf("%d\n", diag->idiag_family);*/
+        /*printf("%d\n", diag->idiag_uid);*/
+        /*printf("%d\n",diag->tcp_info->tcpi_bytes_acked);*/
+
+        int rtalen = nlh->nlmsg_len - NLMSG_LENGTH(sizeof(*diag));
+        // see https://man7.org/linux/man-pages/man7/rtnetlink.7.html
+        struct rtattr *attr = (struct rtattr *)(diag + 1);
+
+        // Walk attributes
+        for (; RTA_OK(attr, rtalen); attr = RTA_NEXT(attr, rtalen)) {
+          if (attr->rta_type == INET_DIAG_INFO) {
+
+            /*
+             * Relevant fields on tcpi structure
+             * tcpi_bytes_acked: total bytes ACKed (delivered).
+             * tcpi_bytes_received: total bytes received.
+             * tcpi_bytes_sent: total bytes sent, including retransmissions.
+             * tcpi_bytes_retrans: total retransmitted bytes.
+             * tcpi_delivery_rate: estimated delivery rate in bytes/sec.
+             */
+            struct tcp_info *tcpi = (struct tcp_info *)RTA_DATA(attr);
+
+            printf("  RTT: %u usec, retrans: %u, cwnd: %u\n", tcpi->tcpi_rtt,
+                   tcpi->tcpi_total_retrans, tcpi->tcpi_snd_cwnd);
+
+            if (len >= offsetof(struct tcp_info, tcpi_bytes_acked) +
+                           sizeof(tcpi->tcpi_bytes_acked)) {
+              printf("  Bytes acked: %llu, Bytes received: %llu\n",
+                     (unsigned long long)tcpi->tcpi_bytes_acked,
+                     (unsigned long long)tcpi->tcpi_bytes_received);
+            }
+          }
+        }
+      }
+    }
+
+    struct timespec time;
+    struct timespec rem;
+    time.tv_sec = 2;
+    time.tv_nsec = 0;
+
+    puts("\n\n");
+    if (nanosleep(&time, &rem) == -1) {
+      perror("sleep time not respected");
+      break;
+    }
+  }
+
+  close(sfd);
 }
 
 // todo
